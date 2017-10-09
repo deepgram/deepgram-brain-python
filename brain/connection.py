435ed724f80c9b32bc56840bc5081d2378446e93
greedyuser <wallyduck@yahoo.com>
2017-10-09 11:02:34 -0700

    More changes for license and push to pypi

--- brain/connection.py
+++ brain/connection.py
@@ -3,7 +3,7 @@ import io
 import base64
 
 """
-Copyright 2016 Deepgram
+Copyright 2017 Deepgram
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
