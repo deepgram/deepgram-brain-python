2ca6481b0be0ca33e800b9665fe5252273f8bcd7
greedyuser <wallyduck@yahoo.com>
2017-10-09 10:34:32 -0700

    License and version updated

--- brain/version.py
+++ brain/version.py
@@ -12,4 +12,4 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from .connection import BrainAPI
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -1,9 +1,35 @@
+
+"""
+Copyright 2016 Deepgram
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+   http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 from setuptools import find_packages, setup
 
 __author__ = 'jmward'
 
+################################################################################
+def get_version():
+	""" Gets the current version of the package.
+	"""
+	version_py = os.path.join(os.path.dirname(__file__), 'brain', 'version.py')
+	with open(version_py, 'r') as fh:
+		for line in fh:
+			if line.startswith('__version__'):
+				return line.split('=')[-1].strip().replace('"', '')
+	raise ValueError('Failed to parse version from: {}'.format(version_py))
+
+################################################################################
+
 setup(name='brain',
-      version='1.0', # use an version include
+      version=get_version(),
       description='Deepgram Brain API client',
       author='Jeff Ward',
       license='apache2', # check KUR for license file
@@ -17,6 +43,8 @@ setup(name='brain',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
       ],
-      keywords='deepgram transcription audio',
+      keywords='deepgram transcription audio deep learning',
       packages=find_packages(),
       install_requires=['requests'])
+
+
